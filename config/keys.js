0efe0308b17f5e877c2d7c23828204b5621f5ee2
Amanda <amanda.baum01@gmail.com>
2018-10-14 22:14:54 -0400

    User and Profile Routes

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://admin:mic530gra@ds231723.mlab.com:31723/audiologydev'
-}
+  mongoURI: 'mongodb://admin:mic530gra@ds231723.mlab.com:31723/audiologydev',
+  secretOrKey: 'secret'
+};
