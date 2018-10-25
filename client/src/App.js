a008679a92a0416400c5769b52962a2b711839c3
Amanda <amanda.baum01@gmail.com>
2018-10-24 22:14:45 -0400

    Profile page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from 'react-redux';
 import store from './store';
+
+import PrivateRoute from './components/common/PrivateRoute';
 
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
@@ -16,6 +18,7 @@ import Landing from './components/layout/landing';
 import Login from "./components/auth/login";
 import Register from "./components/auth/register";
 import Dashboard from "./components/dashboard/dashboard";
+import CreateProfile from "./components/create-profile/CreateProfile";
 
 import './App.css';
 
@@ -51,7 +54,12 @@ class App extends Component {
             <div className="container">
               <Route exact path="/login" component={ Login } />
               <Route exact path="/register" component={ Register } />
-              <Route exact path="/dashboard" component={ Dashboard } />
+              <Switch>
+                <PrivateRoute exact path="/dashboard" component={ Dashboard } />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/create-profile" component={ CreateProfile } />
+              </Switch>
             </div>
             <Footer />
           </div>
