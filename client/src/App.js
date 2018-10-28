92fd09d52c07de5009c74dce3c8a555262eab29f
Amanda <amanda.baum01@gmail.com>
2018-10-28 07:35:14 -0400

    profile complete

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,9 @@ import Login from "./components/auth/login";
 import Register from "./components/auth/register";
 import Dashboard from "./components/dashboard/dashboard";
 import CreateProfile from "./components/create-profile/CreateProfile";
+import EditProfile from "./components/edit-profile/EditProfile";
+import AddExperience from "./components/add-credentials/AddExperience";
+import AddEducation from "./components/add-credentials/AddEducation";
 
 import './App.css';
 
@@ -59,6 +62,15 @@ class App extends Component {
               </Switch>
               <Switch>
                 <PrivateRoute exact path="/create-profile" component={ CreateProfile } />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/edit-profile" component={ EditProfile } />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/add-experience" component={ AddExperience } />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/add-education" component={ AddEducation } />
               </Switch>
             </div>
             <Footer />
